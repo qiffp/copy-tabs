b99498400c6c57a719e8e9e9d81b066b30c45347
Sam Dye <sam.re.dye@gmail.com>
2015-02-15 16:12:33 -0500

    Resize panel to fit content

--- data/panel.js
+++ data/panel.js
@@ -5,6 +5,16 @@ $(function() {
     $("#include-pinned-checkbox").prop("checked", defaults.includePinned);
   });
 
+  self.port.on("get-size", function() {
+    var horizontalPad = $("body").outerWidth(true) - $("body").innerWidth();
+    var verticalPad = $("body").outerHeight(true) - $("body").innerHeight();
+
+    self.port.emit("resize", {
+      width: $("body").width() + horizontalPad,
+      height: $("body").height() + verticalPad
+    });
+  });
+
   $("#separator-input").change(function() {
     self.port.emit("update-settings", {
       setting: "separator",
